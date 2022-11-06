6cde76f2e5ac42ad3225266904d70775a71a0d80
Kareem <Kareem@test.com>
2022-11-06 06:30:41 +0300

    Create Login Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
 function App() {
+  const adminUser = {
+    name: "Kareem",
+    password: "0000",
+  };
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const login = (details) => {
+    console.log(details);
+
+    if (
+      details.name === adminUser.name &&
+      details.password === adminUser.password
+    ) {
+      setUser({
+        name: details.name,
+        password: details.password,
+      });
+    } else {
+      console.log("Error");
+      <div className="error"> {setError("Incorrect Name or Password!")}; </div>;
+    }
+    if (details.name === "" || details.password === "") {
+      setError("All fields are required!!");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", paassword: "" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.name !== "" ? (
+        <div className="login-form">
+          <h2>
+            Welcome
+            <p>
+              {user.name} {""}
+              <p className="success"> Logged in </p>
+            </p>
+          </h2>
+          <button onClick={Logout}> Logout </button>
+        </div>
+      ) : (
+        <LoginForm login={login} error={error} />
+      )}
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function LoginForm({ login, error }) {
+  const [details, setDetails] = useState({ name: "", password: "" });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    login(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="login-form">
+        <h2> Login </h2>
+        {error !== "" ? <div className="error"> {error} </div> : ""}
+        <div style={{ padding: "1rem" }}>
+          <label htmlFor="name"> Name: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+
+        <div style={{ padding: "1rem" }}>
+          <label htmlFor="password"> Password: </label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+
+        <input className="mystyle" type="submit" value="Login" />
+      </div>
+    </form>
+    
+  );
+}
